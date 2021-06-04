02c6720ffb3ed95d905d7f87b3d942b78736fbc5
Oliver Scott <olivercscott@gmail.com>
2021-06-04 10:22:35 -0400

    Add IPv6 handling to dns cleartext allowlist
    Change-Id: I585bddc97c31343e6f8e9222faa913afee187cdc

--- server/StrictController.cpp
+++ server/StrictController.cpp
@@ -215,7 +215,7 @@ int StrictController::setDNSCleartextWhitelist(const std::vector<std::string>& s
     }
     commands.push_back("COMMIT\n");
 
-    return (execIptablesRestore(V4, Join(commands, "\n")) == 0) ? 0 : -EREMOTEIO;
+    return (execIptablesRestore(V4V6, Join(commands, "\n")) == 0) ? 0 : -EREMOTEIO;
 }
 
 inline const char *StrictController::penaltyToString(StrictPenalty penalty) {

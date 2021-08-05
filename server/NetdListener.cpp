a0b01635020eb3cc5fec78e54ce0478bb66dda02
Oliver Scott <olivercscott@gmail.com>
2021-08-04 23:46:43 -0400

    Disallow creating network sockets if uid is restricted
    Create a socket and a corresponding listener to allow netd client to
    query if a UID can access restricted networks and allow/disallow it from
    creating network sockets based on that.
    Allow networking when the restricted firewall chain is disabled.
    
    Issue: calyxos#581
    Change-Id: Iad56cf110bb9da3f63bbe97e6fc77ef10b3e3cba

---
+++ server/NetdListener.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2021 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "NetdListener"
+
+#include <android-base/parseint.h>
+#include <log/log.h>
+#include <netdutils/ResponseCode.h>
+
+#include "NetdListener.h"
+#include "NetdUpdatablePublic.h"
+
+namespace android {
+namespace net {
+
+using android::netdutils::ResponseCode;
+
+NetdListener::NetdListener() : FrameworkListener(SOCKET_NAME) {
+    registerCmd(new Handler());
+}
+
+NetdListener::Handler::Handler() : NetdCommand("netd") {}
+
+NetdListener::Handler::~Handler() {}
+
+
+int NetdListener::Handler::runCommand(SocketClient* cli, int argc, char** argv) {
+    if (argc < 4) {
+        char* msg = nullptr;
+        asprintf(&msg, "Invalid number of arguments to netd: %i", argc);
+        ALOGW("%s", msg);
+        cli->sendMsg(ResponseCode::CommandParameterError, msg, false);
+        free(msg);
+        return -1;
+    }
+
+    if (strcmp(argv[1], "traffic") == 0) {
+        if (strcmp(argv[2], "getuidnetworking") == 0) {
+            cli->sendCode(libnetd_updatable_getNetworkingAllowedForUid(std::stoul(argv[3])));
+        }
+    }
+
+    return 0;
+}
+
+}  // namespace net
+}  // namespace android
